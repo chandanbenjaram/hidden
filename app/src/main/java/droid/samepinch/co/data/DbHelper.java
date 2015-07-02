ca173aabcfb7d29547dfe764d07326c45f7ce506
Chandan Benjaram <chandan.benjaram@gmail.com>
2015-07-02 02:51:34 -0700

    1

--- app/src/main/java/droid/samepinch/co/data/DbHelper.java
+++ app/src/main/java/droid/samepinch/co/data/DbHelper.java
@@ -3,33 +3,45 @@ package droid.samepinch.co.data;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import droid.samepinch.co.data.dao.IPostSchema;
+import droid.samepinch.co.data.dao.IUserSchema;
 
 /**
  * Created by imaginationcoder on 6/24/15.
  */
-public class DbHelper extends SQLiteOpenHelper {
-    static final String DATABASE_NAME = "samepinchapp.db";
-    private static final int DATABASE_VERSION = 1;
+public class DBHelper extends SQLiteOpenHelper {
+    public static final String LOG_TAG = "DBHelper";
 
-    public DbHelper(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    public DBHelper(Context context) {
+        super(context, DB.DATABASE_NAME, null, DB.DATABASE_VERSION);
+    }
+
+    @Override
+    public void onOpen(SQLiteDatabase db) {
+        super.onOpen(db);
+
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        final String SQL_CREATE_POSTS_TABLE = "CREATE TABLE " + WallContract.Post.TABLE_NAME + " (" +
-                WallContract.Post._ID + " INTEGER PRIMARY KEY, " +
-                WallContract.Post.COLUMN_POST_ID + " INTEGER NOT NULL, " +
-                WallContract.Post.COLUMN_CONTENT + " TEXT NOT NULL" +
-                " );";
-
-
-        sqLiteDatabase.execSQL(SQL_CREATE_POSTS_TABLE);
+        sqLiteDatabase.execSQL(IPostSchema.POST_TABLE_CREATE);
+        sqLiteDatabase.execSQL(IUserSchema.USER_TABLE_CREATE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + WallContract.Post.TABLE_NAME);
+        Log.w(LOG_TAG, "Upgrading database from version "
+                + oldVersion + " to "
+                + newVersion + " which destroys all old data");
+
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + IPostSchema.POST_TABLE);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + IUserSchema.USER_TABLE);
+
+        // create table(s)
         onCreate(sqLiteDatabase);
     }
+
+
 }
