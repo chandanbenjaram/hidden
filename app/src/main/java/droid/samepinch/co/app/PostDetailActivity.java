d14cf464cce1e346026e8856fcb39160b71e4835
Chandan Benjaram <chandan.benjaram@gmail.com>
2015-07-28 03:58:53 -0700

    1

--- app/src/main/java/droid/samepinch/co/app/PostDetailActivity.java
+++ app/src/main/java/droid/samepinch/co/app/PostDetailActivity.java
@@ -1,47 +1,35 @@
 package droid.samepinch.co.app;
 
-import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.MergeCursor;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.AttributeSet;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import droid.samepinch.co.app.helpers.CommentsFragment;
-import droid.samepinch.co.app.helpers.Utils;
-import droid.samepinch.co.app.helpers.adapters.PostCursorRecyclerViewAdapter;
 import droid.samepinch.co.app.helpers.adapters.PostDetailsRVAdapter;
 import droid.samepinch.co.app.helpers.intent.PostDetailsService;
-import droid.samepinch.co.app.helpers.widget.SIMView;
 import droid.samepinch.co.data.dao.SchemaComments;
 import droid.samepinch.co.data.dao.SchemaPostDetails;
 import droid.samepinch.co.data.dao.SchemaPosts;
-import droid.samepinch.co.data.dto.PostDetails;
 
 import static droid.samepinch.co.app.helpers.AppConstants.APP_INTENT.KEY_UID;
 
@@ -75,13 +63,6 @@ public class PostDetailActivity extends AppCompatActivity implements CommentsFra
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        TextView dummyView = (TextView) findViewById(R.id.dummy_txt);
-        StringBuilder dummyTxt = new StringBuilder();
-        for(int i=0; i< 5; i++){
-            dummyTxt.append( "\r\n" + "DETAILS..." + i);
-        }
-        dummyView.setText(dummyTxt);
-
         // Get the ActionBar here to configure the way it behaves.
         final ActionBar ab = getSupportActionBar();
 //        ab.setHomeAsUpIndicator(R.drawable.ic_menu); // set a custom icon for the default home button
@@ -90,58 +71,23 @@ public class PostDetailActivity extends AppCompatActivity implements CommentsFra
         ab.setDisplayShowCustomEnabled(false); // enable overriding the default toolbar layout
         ab.setDisplayShowTitleEnabled(false);
 
-//        LinearLayout contentLayout = (LinearLayout) findViewById(R.id.postdetail_content_layout);
+        // query for post details
+        Cursor currPost = getContentResolver().query(SchemaPostDetails.CONTENT_URI, null, SchemaPostDetails.COLUMN_UID + "=?", new String[]{mPostId}, null);
+        // query for post comments
+        Cursor currComments= getContentResolver().query(SchemaComments.CONTENT_URI, null, SchemaComments.COLUMN_POST_DETAILS + "=?", new String[]{mPostId}, null);
 
-
-
-//        PostDetails details = null;
-        Cursor cursor = getContentResolver().query(SchemaPostDetails.CONTENT_URI, null, SchemaPostDetails.COLUMN_UID + "=?", new String[]{mPostId}, null);
-        Cursor c = null;
-        if (StringUtils.isNotBlank(mPostId)) {
-            c = getContentResolver().query(SchemaComments.CONTENT_URI, null, SchemaComments.COLUMN_POST_DETAILS + "=?", new String[]{mPostId}, null);
-        }
-
-        Cursor cursor = getContentResolver().query(SchemaPostDetails.CONTENT_URI, null, SchemaPostDetails.COLUMN_UID + "=?", new String[]{mPostId}, null);
-
-
-        Cursor mergeCursorr = new MergeCursor(new Cursor[]{cursor, c});
-
-
+        // recycler view setup
         RecyclerView rv = (RecyclerView) findViewById(R.id.recyclerView);
         rv.setHasFixedSize(true);
-
         rv.setLayoutManager(mLayoutManager);
 
-        mViewAdapter = new PostDetailsRVAdapter(this, mergeCursorr);
+        mViewAdapter = new PostDetailsRVAdapter(this, new MergeCursor(new Cursor[]{currPost, currComments}));
         rv.setAdapter(mViewAdapter);
         rv.setItemAnimator(new DefaultItemAnimator());
 
-//        if (cursor.moveToFirst()) {
-//            details = Utils.cursorToPostDetailsEntity(cursor);
-//        }
-//        cursor.close();
-//        if (details != null) {
-
-//            // custom recycler
-//            RecyclerView rv = new RecyclerView(getApplicationContext()) {
-//                @Override
-//                public void scrollBy(int x, int y) {
-//                    try {
-//                        super.scrollBy(x, y);
-//                    } catch (NullPointerException nlp) {
-//                        // muted
-//                    }
-//                }
-//            };
-
-
-
-//        }
-
-        // construct context from preferences if any?
+        // prepare to refresh post details
         Bundle iServiceArgs = new Bundle();
         iServiceArgs.putString(KEY_UID.getValue(), mPostId);
-
 
         // call for intent
         mServiceIntent =
